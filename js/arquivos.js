cba3b0cb3285153ae97344cbe3ea2542a9f92b7e
Romilson Monteiro <romilson.tricolor@gmail.com>
2022-06-08 20:48:15 -0300

    Finalizado

--- js/arquivos.js
+++ js/arquivos.js
@@ -1,10 +1,9 @@
 function CalcularIMC(){
-    let peso = $('peso');
-    let altura = $('altura');
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
     let imc = 0;
     let resultado;
 
-    console.log(nome);
     console.log(peso);
     console.log(altura);
 
@@ -29,9 +28,72 @@ function CalcularIMC(){
     else{
         resultado = "Obesidade Grau 3"
     }
+    console.log(imc)
+    console.log(resultado)
+    
+    let erro = "";
+    
+        if(document.getElementById("peso").value.trim() == ""){
+            erro = "- O campo peso é obrigatório!<br>"
+        }
+    
+        if(document.getElementById("altura").value.trim() ==""){
+            erro += "- O campo altura é obrigatório!<br>";
+        }
 
-    //document.write(nome + "possui o IMC " + imc + " e está " + resultado)
-    //ou
-    alert(nome + " possui o IMC " + imc + " e está " + resultado)
+        if (erro != ""){
+            //coloca o conteúdo na div erro
+            $('#erro').html("ATENÇÃO!<br><br>" + erro);
+            //exibe o modal
+            $('#erro').modal({
+                fadeDuration: 1000
+            });
+    
+            return false;
+        }
+        else{
+            $('#resultado').html("IMC<br><br>" + imc +"<br>" + resultado + "<br>");
+            $('#resultado').modal({
+                fadeDuration: 1000
+            });
+        }
 }
 
+$(document).ready(function(){
+    $('.peso').mask('000.000', {reverse: true});
+    $('.altura').mask('000.00', {reverse: true});
+	$('.date').mask('00/00/0000');
+	$('.time').mask('00:00:00');
+	$('.date_time').mask('00/00/0000 00:00:00');
+	$('.cep').mask('00000-000');
+	$('.phone').mask('0000-0000');
+	$('.phone_with_ddd').mask('(00) 0000-0000');
+	$('.license_plate').mask('SSS-0A00');
+	$('.phone_us').mask('(000) 000-0000');
+	$('.mixed').mask('AAA 000-S0S');
+	$('.cpf').mask('000.000.000-00', {reverse: false});
+	$('.cnpj').mask('00.000.000/0000-00', {reverse: true});
+	$('.money').mask('000.000.000.000.000,00', {reverse: true});
+	$('.money2').mask("#.##0,00", {reverse: true});
+	$('.ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
+	  translation: {
+		'Z': {
+		  pattern: /[0-9]/, optional: true
+		}
+	  }
+	});
+	$('.ip_address').mask('099.099.099.099');
+	$('.percent').mask('##0,00%', {reverse: true});
+	$('.clear-if-not-match').mask("00/00/0000", {clearIfNotMatch: true});
+	$('.placeholder').mask("00/00/0000", {placeholder: "__/__/____"});
+	$('.fallback').mask("00r00r0000", {
+		translation: {
+		  'r': {
+			pattern: /[\/]/,
+			fallback: '/'
+		  },
+		  placeholder: "__/__/____"
+		}
+	  });
+	$('.selectonfocus').mask("00/00/0000", {selectOnFocus: true});
+  });
